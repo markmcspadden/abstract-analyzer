74b758f434b1cebfb296daf4314eb6955627fb62
Mark McSpadden <markmcspadden@gmail.com>
2009-10-09 14:08:39 -0500

    Update implementation notes for Rails middleware

--- lib/middleware/rails.rb
+++ lib/middleware/rails.rb
@@ -8,8 +8,12 @@ module AbstractAnalyzer
     #   require 'abstract_analyzer/abstract_analyzer'
     #
     #   # Setup the Analyzer DB
-    #   mongo_analyzer_db = Mongo::Connection.new('localhost', 27017).db('aa-rails-dash-analyzer-db')
-    #   AbstractAnalyzer.const_set("DB", mongo_analyzer_db)
+    #   abstract_analyzer_db = Mongo::Connection.new('localhost', 27017).db('aa-rails-dash-analyzer-db')
+    #   AbstractAnalyzer.const_set("DB", abstract_analyzer_db)
+    # 
+    #   # Setup the Analyzer LOGGER
+    #   abstract_logger = Logger.new("#{RAILS_ROOT}/log/abstract_analyzer_logger.log")
+    #   AbstractAnalyzer.const_set("LOGGER", abstract_logger)
     # 
     #   # Use the Analyzer and View middlewares
     #   ActionController::Dispatcher.middleware.use AbstractAnalyzer::Middleware::Rails::Dash::Analyzer
