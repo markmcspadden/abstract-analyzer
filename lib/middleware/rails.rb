c3bab646a5d03f7f598af07e7ee930deb9db9b86
Mark McSpadden <markmcspadden@gmail.com>
2009-10-09 12:35:20 -0500

    Add some implementation details to the Rails middleware

--- lib/middleware/rails.rb
+++ lib/middleware/rails.rb
@@ -2,6 +2,14 @@ require File.dirname(__FILE__) << '/rails/dash'
 
 module AbstractAnalyzer
   module Middleware
+    # Implementation
+    # In RAILS_ROOT/config/initializers/middlewares.rb
+    #
+    #   require 'abstract_analyzer/abstract_analyzer'
+    #   ActionController::Dispatcher.middleware.use AbstractAnalyzer::Middleware::Rails::Dash::Analyzer
+    #   ActionController::Dispatcher.middleware.use AbstractAnalyzer::Middleware::Rails::Dash::View
+    #
+    # NOTE: The require piece will be replace by an environment.rb config.gem call at some point
     module Rails
     end
   end
